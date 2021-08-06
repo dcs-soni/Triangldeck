b62adee5026f4bff37603f1bdb18cc652ab5a90e
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2021-08-06 21:48:07 +0530

    refactor: change in logic and modify form elements

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -5,27 +5,49 @@ const boxes = document.querySelectorAll(".box");
 const result = document.querySelector(".result");
 // console.log(boxes);
 
+// console.log(questions);
 function showResults() {
-    const correctAnswers = ["question1c", "question2d", "question3a", "question4b", "question5b", "question6b", "question7c", "question8c", "question9b", "question10b" ]
+    const correctAnswers = ["question1c", "question2d", "question3a", "question4b", "question5b", "question6b", "question7c", "question8c", "question9b", "question10b" ];
 
-    console.log(questions);
+    // console.log(questions);
     let score = 0;
-    for(let i = 0; i< userInput.length; i++){
-        let index = correctAnswers.indexOf(userInput[i].id);
-
-        if(userInput[i].checked){
-            if(correctAnswers.includes(userInput[i].id)){
-                score += 1;
-                boxes[index].style.backgroundColor = "green";
-            }else {
-                console.log("error");
-                // document.getElementById(`${userInput[i].id}`).style.backgroundColor = "red";
-            }
-        }   
-    }
+    // for(let i = 0; i< userInput.length; i++){
+    //     let index = correctAnswers.indexOf(userInput[i].id);
+    //     console.log(index);
+    //     if(userInput[i].checked){
+    //         if(correctAnswers.includes(userInput[i].id)){
+    //             score += 1;
+    //             boxes[index].style.backgroundColor = "green";
+    //         }else {
+    //             console.log("error");
+    //             // document.getElementById(`${userInput[i].id}`).style.backgroundColor = "red";
+    //         }
+    //     }   
+    // }
     
-    console.log(score);
+    let userAnswers = []
+    for(answer of userInput){
+        console.log(answer);
+        if(answer.checked){
+            userAnswers.push(answer.id);
+        }
+    }
+
+    console.log(userAnswers);
+
+
+    for(index in userAnswers){
+        if(correctAnswers.includes(userAnswers[index])) {
+            score += 1;
+            boxes[index].style.backgroundColor = "green";
+        }
+        else {
+         boxes[index].style.backgroundColor = "red";
+         }
+    }
+
     result.innerText = `Your score is ${score}`;
+    console.log(score);
 
 }
 
